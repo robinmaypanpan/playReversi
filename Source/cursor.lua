7ef6321fcfc444dfa0298ca99f62ac351a5398ce
Robin Mayorga <robinmaypanpan@gmail.com>
2022-11-28 03:04:01 +0000

    Remove deprecated cursor code

--- Source/cursor.lua
+++ Source/cursor.lua
@@ -6,32 +6,12 @@ local gfx = playdate.graphics
 
 class('Cursor').extends(AnimatedSprite)
 
-function Cursor:init(spaceSize)	
+function Cursor:init()	
 	Cursor.super.init(self, gfx.imagetable.new('assets/images/cursor'))
-	self.spaceSize = spaceSize;
 	
 	self:addState('valid', 1, 3, {tickStep = 5, yoyo = true})
 	self:addState('invalid', 4, 5, {tickStep = 13})
 	self:setZIndex(20)
-end
-
-function Cursor:drawCursor()
-	local cursorSize = self.spaceSize + 20
-	local cursorImage = gfx.image.new(cursorSize, cursorSize)
-	gfx.pushContext(cursorImage)		
-		gfx.setColor(gfx.kColorBlack)
-		
-		if (self.isValid == false) then
-			gfx.setDitherPattern(0.4)
-			gfx.fillRect(0,0,cursorSize, cursorSize)
-		end
-		
-		gfx.setDitherPattern(0)
-		gfx.setLineWidth(10)
-		gfx.drawRect(0, 0, cursorSize, cursorSize)
-		
-	gfx.popContext()
-	return cursorImage
 end
 
 function Cursor:setValidPosition(isValid)
